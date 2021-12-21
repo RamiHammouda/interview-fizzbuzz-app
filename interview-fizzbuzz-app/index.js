17bf4cd214eb3bcc26c6ed2cb7342fae91a20638
RamiHammouda <96247702+RamiHammouda@users.noreply.github.com>
2021-12-21 13:14:54 +0100

    Update index.js

--- interview-fizzbuzz-app/index.js
+++ interview-fizzbuzz-app/index.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const sum = require('./src/FizzBuzz'); //import function
 
 //Our function
-function solve(i) {
-  return i % 3 == 0
-    ? i % 5 == 0
-      ? 'FizzBuzz'
-      : 'Fizz'
-    : i % 5 == 0
-    ? 'Buzz'
-    : i;
-}
+// function solve(i) {
+//   return i % 3 == 0
+//     ? i % 5 == 0
+//       ? 'FizzBuzz'
+//       : 'Fizz'
+//     : i % 5 == 0
+//     ? 'Buzz'
+//     : i;
+// }
 
 // log to port 3000 'can be deleted'
 // This responds with "Hello World" on the homepage
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 // log to console
 //listening to port 3000 for a connection
 app.listen(port, () => {
-   //loop used to log the generated fizzbuzz numbers from 1 to 100
+  //loop used to log the generated fizzbuzz numbers from 1 to 100
   for (var i = 1; i <= 100; i++) {
-    console.log(solve(i));
+    console.log(sum.solve(i));
   }
 });
